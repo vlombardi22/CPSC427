bf026b9d939381aa15377b2c9704e87ffd7751ce
Qwertie64982 <maxwell.a.sherman@live.com>
2019-03-22 14:55:09 -0700

    Minor formatting changes for consistency

--- Project 8/Mate.java
+++ Project 8/Mate.java
@@ -87,32 +87,41 @@ public class Mate
             char MT_father_gene2 = MT_father.GetGene(crossPoint2);
 
             if (MT_mother_gene1 != MT_father_gene1 &&
-                    MT_mother_gene2 != MT_father_gene2) {
+                    MT_mother_gene2 != MT_father_gene2)
+            {
                 //where the matching genes are in the other parent
                 int MT_mother_loc1 = 0; //index of father gene1 in mother
-                for (int i = 0; i < MT_mother.GetNumGenes(); i++) {
-                    if (MT_mother.GetGene(i) == MT_father_gene1) {
+                for (int i = 0; i < MT_mother.GetNumGenes(); i++)
+                {
+                    if (MT_mother.GetGene(i) == MT_father_gene1)
+                    {
                         MT_mother_loc1 = i;
                         break;
                     }
                 }
                 int MT_mother_loc2 = 0; //index of father gene2 in mother
-                for (int i = 0; i < MT_mother.GetNumGenes(); i++) {
-                    if (MT_mother.GetGene(i) == MT_father_gene2) {
+                for (int i = 0; i < MT_mother.GetNumGenes(); i++)
+                {
+                    if (MT_mother.GetGene(i) == MT_father_gene2)
+                    {
                         MT_mother_loc2 = i;
                         break;
                     }
                 }
                 int MT_father_loc1 = 0; //index of mother gene1 in father
-                for (int i = 0; i < MT_father.GetNumGenes(); i++) {
-                    if (MT_father.GetGene(i) == MT_mother_gene1) {
+                for (int i = 0; i < MT_father.GetNumGenes(); i++)
+                {
+                    if (MT_father.GetGene(i) == MT_mother_gene1)
+                    {
                         MT_father_loc1 = i;
                         break;
                     }
                 }
                 int MT_father_loc2 = 0; //index of mother gene1 in father
-                for (int i = 0; i < MT_father.GetNumGenes(); i++) {
-                    if (MT_father.GetGene(i) == MT_mother_gene2) {
+                for (int i = 0; i < MT_father.GetNumGenes(); i++)
+                {
+                    if (MT_father.GetGene(i) == MT_mother_gene2)
+                    {
                         MT_father_loc2 = i;
                         break;
                     }
@@ -178,8 +187,8 @@ public class Mate
             MT_child2 = new Chromosome(MT_numGenes);
 
             Random rnum = new Random();
-            // calculates crossover points so that they are an acceptable distance from each other
-            // and the beginning and end of the list
+            //calculates crossover points so that they are an acceptable distance from each other
+            //and the beginning and end of the list
             int crossPoint1 = rnum.nextInt(MT_numGenes - 2);
             if (crossPoint1 == 0)
             {
@@ -201,13 +210,13 @@ public class Mate
             ArrayList<Character> child2 = new ArrayList<>();
             ArrayList<Character> temp1 = new ArrayList<>();
             ArrayList<Character> temp2 = new ArrayList<>();
-            // puts genes into an array
+            //puts genes into an array
             for (int i = 0; i < MT_numGenes; i++)
             {
                 child1.add(MT_father.GetGene(i));
                 child2.add(MT_mother.GetGene(i));
             }
-            // puts elements that need to be swapped into separate array lists
+            //puts elements that need to be swapped into separate array lists
             for (int i = crossPoint1; i < crossPoint2; i++)
             {
                 temp1.add(MT_mother.GetGene(i));
@@ -216,7 +225,7 @@ public class Mate
                 //child2.remove(MT_mother.GetGene(i));
             }
 
-            // deletes repeat elements from child arrays
+            //deletes repeat elements from child arrays
             for (int i = 0; i < temp1.size(); i++)
             {
                 if(child1.contains(temp1.get(i))){
@@ -232,7 +241,7 @@ public class Mate
             int count1 = 0;
             int count2 = 0;
 
-            // puts all elements before crossoverpoint 1 back into child1 and child2
+            //puts all elements before crossoverpoint 1 back into child1 and child2
             for (int i = 0; i < crossPoint1; i++)
             {
                 MT_child1.SetGene(count1, child1.get(i));
@@ -240,7 +249,7 @@ public class Mate
                 count1++;
                 count2++;
             }
-            // puts crossover elements into child1 and child2
+            //puts crossover elements into child1 and child2
             for (int i = 0; i < temp1.size(); i++)
             {
                 MT_child1.SetGene(count1, temp1.get(i));
@@ -249,7 +258,7 @@ public class Mate
                 count2++;
             }
 
-            // puts all elements after crossover points back into child1 and child2
+            //puts all elements after crossover points back into child1 and child2
             for (int i = crossPoint1; i < child1.size(); i++)
             {
                 MT_child1.SetGene(count1, child1.get(i));
@@ -293,19 +302,20 @@ public class Mate
             ArrayList<Character> child1 = new ArrayList<>();
             ArrayList<Character> child2 = new ArrayList<>();
 
-            // puts parent elements into arrays
+            //puts parent elements into arrays
             for (int i = 0; i < MT_numGenes; i++)
             {
                 child1.add(MT_father.GetGene(i));
                 child2.add(MT_mother.GetGene(i));
             }
-            // preform initial swap
+            //perform initial swap
             char temp;
             temp = child1.get(0);
             child1.set(0, child2.get(0));
             child2.set(0, temp);
             int index = 0;
-            // cycle through child lists swapping repeat elements till there are none
+
+            //cycle through child lists swapping repeat elements till there are none
             while(index != -1)
             {
                 temp = child1.get(index);
@@ -313,12 +323,13 @@ public class Mate
                 child2.set(index, temp);
                 index = repeatChecker(child1);
             }
-            // put elements back into child object
+            //put elements back into child object
             for(int i = 0; i < child1.size(); i++)
             {
                 MT_child1.SetGene(i, child1.get(i));
                 MT_child2.SetGene(i, child2.get(i));
             }
+
             /*
             System.out.println("child1");
             //System.out.println("");
@@ -333,7 +344,6 @@ public class Mate
             System.out.println("");
             */
 
-
             population.add(MT_posChild1,MT_child1);
             population.add(MT_posChild2,MT_child2);
             MT_posChild1    = MT_posChild1 + 2;
@@ -341,11 +351,10 @@ public class Mate
             MT_posFather    = MT_posFather + 2;
             MT_posMother    = MT_posMother + 2;
         }
-
         return population;
     }
 
-    // helper function that checks for repeats
+    //helper function that checks for repeats
     private int repeatChecker(ArrayList<Character> input)
     {
         for (int x = 0; x < input.size(); x++)
